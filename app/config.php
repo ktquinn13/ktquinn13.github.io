780125c68b248add36516b57d08928f8f16f67e7
Taylor Quinn <ktquinn13@gmail.com>
2017-01-01 12:13:56 -0500

    FIxing the baseURL

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://ktquinn13.github.io');
+define('BASE_URL','');
 
 // database constants
 define('DB_HOST','127.0.0.1');
